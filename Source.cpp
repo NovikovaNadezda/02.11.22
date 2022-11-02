091c2e2955d8ca5ce6d16e70115e03c76071bae3
NovikovaNadezda <114093562+NovikovaNadezda@users.noreply.github.com>
2022-11-02 18:33:34 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*const int n = 5;
+	int mas[n];
+	for (int i = 0; i < n; i++) {
+		cin >> mas[i];
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		cout << mas[i] << ' ';
+	}
+	cout << endl;*/
+	/*const int n = 15;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 21;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	int kol_vo = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 == 0) {
+			kol_vo++;
+		}
+	}
+	cout << kol_vo << endl;*/
+	//1
+	/*const int n = 15;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 21;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	int kol_vo = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 5 != 0) {
+			kol_vo++;
+		}
+	}
+	cout << kol_vo << endl;*/
+	//2
+	/*const int n = 20;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 31;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	int kol_vo = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 5== 0&& arr[i]%3==0) {
+			kol_vo++;
+		}
+	}
+	cout << kol_vo << endl;*/
+	//3
+	/*const int n = 17;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 46;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	int long long p = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 == 0 || arr[i] % 3 == 0) {
+			long long p=pow(arr[i],17)
+		}
+	}
+	cout << kol_vo << endl;*/
+	//4
+	/*const int n = 17;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 53;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	int sr = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 == 0 || arr[i] % 3 == 0) {
+		   sr+arr[i]/17;
+		}
+	}
+	cout << sr << endl;*/
+	//5
+
+
+}
